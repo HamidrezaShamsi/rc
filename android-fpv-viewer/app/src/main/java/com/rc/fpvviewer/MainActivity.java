ac0e344a32b1d7845ee18b2f1d26406126d6f629
hamidrezashamsi <hamidreza.shamsi@gmail.com>
2026-03-14 15:50:26 -0400

    Fix LibVLC IVLCVout import path for Android build.
    Use the interfaces package import required by libvlc-all so the CI APK compilation succeeds.

--- android-fpv-viewer/app/src/main/java/com/rc/fpvviewer/MainActivity.java
+++ android-fpv-viewer/app/src/main/java/com/rc/fpvviewer/MainActivity.java
@@ -13,10 +13,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.videolan.libvlc.IVLCVout;
 import org.videolan.libvlc.LibVLC;
 import org.videolan.libvlc.Media;
 import org.videolan.libvlc.MediaPlayer;
+import org.videolan.libvlc.interfaces.IVLCVout;
 
 import java.util.ArrayList;
 
